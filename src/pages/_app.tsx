bd6d5bb42a06ead217080ba4230fcc5ea585cb32
Martin Cajthaml <martin.cajthaml@symbio.agency>
2020-04-10 23:47:02 +0200

    WIP

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import { AppInitialProps } from 'next/dist/next-server/lib/utils';
 import App, { AppContext } from 'next/app';
 import symbio from '../../symbio.config';
 import { basicAuth } from '../lib/auth/basicAuth';
+import '../styles/global.scss';
 
 export default class MyApp extends App {
     public static async getInitialProps(ctx: AppContext): Promise<AppInitialProps> {
