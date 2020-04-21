dec26c0fb8d40e8d923ee8070cb4b287de2e95a8
Martin Cajthaml <martin.cajthaml@symbio.agency>
2020-04-21 10:47:20 +0200

    Sizes of headings in rem

--- src/components/Heading/Heading.tsx
+++ src/components/Heading/Heading.tsx
@@ -4,17 +4,38 @@ import styles from './Heading.module.scss';
 
 export interface HeadingProps {
     tag: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6';
+    size?: 'xl' | 'lg' | 'md' | 'sm' | 'xs';
     className?: string;
     children: ReactNode;
 }
 
+function getSizeFromTag(tag: string): string {
+    switch (tag) {
+        case 'h1':
+            return 'xl';
+        case 'h2':
+            return 'lg';
+        case 'h3':
+            return 'md';
+        case 'h4':
+            return 'sm';
+        case 'h5':
+        case 'h6':
+            return 'xs';
+        default:
+            return 'md';
+    }
+}
+
 export const Heading = ({
     tag,
+    size,
     className,
     children,
 }: HeadingProps): ReactElement<HeadingProps, HeadingProps['tag']> | null => {
     const CustomTag = tag;
-    const classes = [styles.heading];
+    const realSize = size || getSizeFromTag(tag);
+    const classes = [styles[realSize]];
     className && classes.push(className);
 
     return <CustomTag className={classes.join(' ')}>{children}</CustomTag>;
