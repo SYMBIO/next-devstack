74003137e3a588291e336a45ae0b0227567bb35f
Martin Cajthaml <martin.cajthaml@symbio.agency>
2020-07-17 14:11:07 +0200

    Update preview.ts

--- src/pages/api/preview.ts
+++ src/pages/api/preview.ts
@@ -1,16 +1,17 @@
 import { NextApiRequest, NextApiResponse } from 'next';
+import symbio from '../../../symbio.config.json';
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 function getDestination({ locale, type, id, slug, url }: Record<string, string>): string {
     switch (type) {
-        case '217390':
+        case symbio.datocms.pageTypeId:
             return `/${locale}/${url}`;
     }
 
     return `/${locale}`;
 }
 
-export default async (req: NextApiRequest, res: NextApiResponse): Promise<void> => {
+export default (req: NextApiRequest, res: NextApiResponse): void => {
     const locale = String(req.query.locale);
     const type = String(req.query.type);
     const id = String(req.query.id);
