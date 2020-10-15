97e8c3a9f144be136111f2d6fde70a672602eecb
Ondřej Zeman <ondrej.zeman@symbio.agency>
2020-10-15 10:31:28 +0200

    build fix

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,16 +1,7 @@
-import { GetStaticProps } from 'next';
 import React, { ReactElement } from 'react';
 
 const Page = (): ReactElement => {
     return <span>404</span>;
-};
-
-export const getStaticProps: GetStaticProps = async () => {
-    return {
-        props: {
-            code: 404,
-        },
-    };
 };
 
 export default Page;
