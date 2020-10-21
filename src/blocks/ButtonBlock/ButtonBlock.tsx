c791256155e6300b3193a447619b471719e269d5
Ondřej Zeman <ondrej.zeman@symbio.agency>
2020-10-21 10:05:07 +0200

    createBlocks update

--- src/blocks/ButtonBlock/ButtonBlock.tsx
+++ src/blocks/ButtonBlock/ButtonBlock.tsx
@@ -1,15 +1,27 @@
 import React, { ReactElement } from 'react';
 import { graphql } from 'react-relay';
 import { BlockWrapper } from '../../components/base/BlockWrapper/BlockWrapper';
-import { Button } from '../../components/primitives/Button/Button';
 import { BaseBlockProps } from '../../types/block';
+import condCls from '../../utils/conditionalClasses';
 import styles from './ButtonBlock.module.scss';
+import { ButtonBlock_content } from './__generated__/ButtonBlock_content.graphql';
+
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+interface ServerProps {}
+
+type ButtonBlockProps = ServerProps & {
+    content: ButtonBlock_content;
+    className?: string;
+};
 
 graphql`
     fragment ButtonBlock_content on ButtonBlockRecord {
         id
         file {
-            ...appImageFragment @relay(mask: false)
+            id
+            size
+            title
+            url
         }
         icon {
             id
@@ -21,14 +33,26 @@ graphql`
     }
 `;
 
-function ButtonBlock({ content, ...rest }: BaseBlockProps): ReactElement<BaseBlockProps, 'BaseBlock'> {
+function ButtonBlock({ content, className, ...rest }: ButtonBlockProps): ReactElement<BaseBlockProps, 'BaseBlock'> {
     return (
-        <BlockWrapper tooltip={'ButtonBlock'} className={styles.wrapper} {...rest}>
-            <Button icon={content.icon} href={content.page}>
-                {content.label}
-            </Button>
+        <BlockWrapper tooltip={'ButtonBlock'} className={condCls(styles.wrapper, className)} {...rest}>
+            <div>ButtonBlock: {JSON.stringify(content)}</div>
         </BlockWrapper>
     );
+}
+
+if (typeof window === 'undefined') {
+    // put your getStaticProps or getStaticPaths
+    /*
+    ButtonBlock.getStaticProps = async ({
+        locale,
+        providers,
+    }: StaticBlockContext): Promise<ServerProps> => {
+        const provider = providers.x;
+
+        return {};
+    };
+    */
 }
 
 ButtonBlock.whyDidYouRender = true;
