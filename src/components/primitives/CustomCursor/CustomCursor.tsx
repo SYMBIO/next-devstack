dd3975c13c59121e42cff37c8e3e3c969cd910b1
Jan Tesárek <jan.tesarek@symbio.agency>
2020-11-05 17:30:07 +0100

    Add custom cursor

--- src/components/primitives/CustomCursor/CustomCursor.tsx
+++ src/components/primitives/CustomCursor/CustomCursor.tsx
@@ -1,12 +1,28 @@
 import React, { ReactElement, ReactNode, useEffect, useRef } from 'react';
 import styles from './CustomCursor.module.scss';
 
+const BIG_SIZE = '3rem';
+const SMALL_SIZE = '1.5rem';
+const HOVER_BG_COLOR = '#ff6600';
+const DEFAULT_BG_COLOR = 'transparent';
+
 interface CustomCursorProps {
     children?: ReactNode;
 }
 
 export const CustomCursor = ({ children }: CustomCursorProps): ReactElement => {
     const cursorRef = useRef<HTMLDivElement>(null);
+    let clickables: NodeListOf<Element> | [] = [];
+
+    const setBackground = (color: string) => {
+        if (cursorRef && cursorRef.current) {
+            cursorRef.current.style.background = color;
+            cursorRef.current.style.width = BIG_SIZE;
+            cursorRef.current.style.height = BIG_SIZE;
+        }
+    };
+    const setTransparentBackground = () => setBackground(DEFAULT_BG_COLOR);
+    const setSolidBackground = () => setBackground(HOVER_BG_COLOR);
 
     const handleMouseMove = (e: MouseEvent) => {
         if (cursorRef && cursorRef.current) {
@@ -15,27 +31,53 @@ export const CustomCursor = ({ children }: CustomCursorProps): ReactElement => {
         }
     };
 
-    const handleMouseEnter = (e: MouseEvent) => {
-        alert('enter');
+    const handleMouseDown = () => {
+        if (cursorRef && cursorRef.current) {
+            cursorRef.current.style.width = SMALL_SIZE;
+            cursorRef.current.style.height = SMALL_SIZE;
+        }
     };
 
-    const handleMouseClick = (e: MouseEvent) => {
-        alert('click');
+    const handleMouseUp = () => {
+        if (cursorRef && cursorRef.current) {
+            cursorRef.current.style.width = BIG_SIZE;
+            cursorRef.current.style.height = BIG_SIZE;
+        }
     };
 
     const addEventListeners = () => {
         window.addEventListener('mousemove', handleMouseMove);
-        window.addEventListener('mousedown', handleMouseClick);
-        window.addEventListener('mouseenter', handleMouseEnter);
+        window.addEventListener('mousedown', handleMouseDown);
+        window.addEventListener('click', handleMouseUp);
+        window.addEventListener('mouseup', handleMouseUp);
+
+        clickables.forEach((el: Element) => {
+            el.addEventListener('mouseover', setSolidBackground);
+            el.addEventListener('mouseout', setTransparentBackground);
+        });
     };
 
     const removeEventListeners = () => {
         window.removeEventListener('mousemove', handleMouseMove);
-        window.removeEventListener('mousedown', handleMouseClick);
-        window.removeEventListener('mouseenter', handleMouseEnter);
+        window.removeEventListener('mousedown', handleMouseDown);
+        window.removeEventListener('click', handleMouseUp);
+        window.removeEventListener('mouseup', handleMouseUp);
+
+        clickables.forEach((el: Element) => {
+            el.removeEventListener('mouseover', setSolidBackground);
+            el.removeEventListener('mouseout', setTransparentBackground);
+        });
     };
 
     useEffect(() => {
+        clickables = document.querySelectorAll('a, input[type="submit"], label[for], select, button, .link');
+
+        if (cursorRef && cursorRef.current) {
+            cursorRef.current.style.width = BIG_SIZE;
+            cursorRef.current.style.height = BIG_SIZE;
+            document.body.style.cursor = 'none';
+        }
+
         addEventListeners();
         return () => removeEventListeners();
     }, []);
