d92907a99b743a5b070cff3c75bdc049e6051358
Jan Tesárek <jan.tesarek@symbio.agency>
2020-11-05 17:35:03 +0100

    Remove from layout

--- src/components/base/Layout/Layout.tsx
+++ src/components/base/Layout/Layout.tsx
@@ -1,16 +1,10 @@
 import React, { ReactElement, ReactNode } from 'react';
 import styles from './Layout.module.scss';
-import { CustomCursor } from '../../primitives/CustomCursor/CustomCursor';
 
 interface LayoutProps {
     children: ReactNode;
 }
 
 export const Layout = ({ children }: LayoutProps): ReactElement<LayoutProps, 'div'> | null => {
-    return (
-        <div className={styles.grid}>
-            {children}
-            <CustomCursor />
-        </div>
-    );
+    return <div className={styles.grid}>{children}</div>;
 };
