0a76aa33e464c325d72f2c413b2f83970b1c89d3
Martin Cajthaml <martin.cajthaml@symbio.agency>
2020-11-11 12:31:31 +0100

    Remove cursor on element with custom cursor

--- src/components/primitives/CustomCursor/CustomCursorProvider.tsx
+++ src/components/primitives/CustomCursor/CustomCursorProvider.tsx
@@ -1,32 +1,28 @@
-import React, { ReactElement, ReactNode, RefObject, useEffect, useState } from 'react';
+import React, { ReactElement, ReactNode, useEffect, useState } from 'react';
 import { CursorContext } from '../../../contexts/cursor-context/CursorContext';
 import { CursorEvents } from '../../../types/cursorContext';
-
-interface CustomCursorRendererProps {
-    ref: RefObject<HTMLDivElement>;
-    children: ReactNode;
-}
 
 interface CustomCursorProviderProps {
     children: ReactNode;
 }
 
-const CustomCursorRenderer = ({ ref, children }: CustomCursorRendererProps) => {
-    return (
-        <div
-            ref={ref}
-            style={{
-                position: 'absolute',
-                border: 'solid 1px red',
-                zIndex: 100000,
-                backgroundColor: 'blue',
-            }}
-            className={'cursor'}
-        >
-            {children}
-        </div>
-    );
-};
+// eslint-disable-next-line react/display-name
+const CustomCursorRenderer = React.forwardRef<HTMLDivElement, CustomCursorProviderProps>((props, ref) => (
+    <div
+        ref={ref}
+        style={{
+            position: 'fixed',
+            border: 'solid 1px red',
+            zIndex: 100000,
+            backgroundColor: 'blue',
+            pointerEvents: 'none',
+        }}
+        className={'cursor'}
+    >
+        {/* eslint-disable-next-line react/prop-types */}
+        {props.children}
+    </div>
+));
 
 export const CustomCursorProvider = ({ children }: CustomCursorProviderProps): ReactElement => {
     const [cursor, setCursor] = useState<Array<React.FC<any>>>([]);
@@ -35,6 +31,7 @@ export const CustomCursorProvider = ({ children }: CustomCursorProviderProps): R
     const buildEvents = (componentEl: HTMLElement): CursorEvents<any> => ({});
 
     const addCursor = (componentEl: HTMLElement, component: React.FC<any>) => {
+        componentEl.classList.add('noCursor');
         componentEl.addEventListener('mouseenter', handleMouseEnter);
         componentEl.addEventListener('mouseleave', handleMouseLeave);
         return buildEvents(componentEl);
@@ -42,6 +39,7 @@ export const CustomCursorProvider = ({ children }: CustomCursorProviderProps): R
 
     // eslint-disable-next-line @typescript-eslint/no-empty-function
     const removeCursor = (componentEl: HTMLElement) => {
+        componentEl.style.removeProperty('cursor');
         componentEl.removeEventListener('mouseenter', handleMouseEnter);
         componentEl.removeEventListener('mouseleave', handleMouseLeave);
     };
