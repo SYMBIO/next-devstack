b8902b807c7afde928ecdf8ccf960088fe27e74c
Martin Cajthaml <martin.cajthaml@symbio.agency>
2021-06-09 09:17:43 +0200

    Exports in cms package

---
+++ packages/cms/providers/index.ts
@@ -0,0 +1,7 @@
+import AbstractProvider from './AbstractProvider';
+import AbstractSingletonProvider from './AbstractSingletonProvider';
+
+export default {
+    AbstractProvider,
+    AbstractSingletonProvider,
+};
