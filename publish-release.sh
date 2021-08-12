361350b7a2bfee52eb09fe9cc0d41de727a59d0d
ozeman <ondrej.zeman@symbio.agency>
2021-08-12 15:53:41 +0200

    --no-verify-access

--- publish-release.sh
+++ publish-release.sh
@@ -17,7 +17,7 @@ echo "//registry.npmjs.org/:_authToken=$NPM_TOKEN" >> ~/.npmrc
 if [[ $(git describe --exact-match 2> /dev/null || :) =~ -canary ]];
 then
   echo "Publishing canary"
-  yarn run lerna publish from-git --npm-tag canary --yes
+  yarn run lerna publish from-git --npm-tag canary --yes --no-verify-access
 
   # Make sure to exit script with code 1 if publish failed
   if [[ ! $? -eq 0 ]];then
@@ -29,7 +29,7 @@ fi
 
 if [[ ! $(git describe --exact-match 2> /dev/null || :) =~ -canary ]];then
   echo "Publishing stable"
-  yarn run lerna publish from-git --yes
+  yarn run lerna publish from-git --yes --no-verify-access
 
   # Make sure to exit script with code 1 if publish failed
   if [[ ! $? -eq 0 ]];then
