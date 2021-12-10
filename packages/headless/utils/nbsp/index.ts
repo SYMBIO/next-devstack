5c47516623dedd33aab81e8f35a2c561c6483717
ozeman <ondrej.zeman@symbio.agency>
2021-12-10 13:08:39 +0100

    nbsp update

--- packages/headless/utils/nbsp/index.ts
+++ packages/headless/utils/nbsp/index.ts
@@ -1,3 +1,9 @@
 export default function nbsp(txt: string | null | undefined): string | undefined {
-    return txt?.replace(/(\s|^)(\w)(\s)(\w)/g, '$1$2\u00A0$4');
+    return txt
+        ?.replace(
+            /(\s|^)((["'„])*[_A-Za-z0-9\u0100-\u017F])((\s)([_A-Za-z0-9\u0100-\u017F]))+/gu,
+            (match, p1) => p1 + match.substr(p1.length).replace(/\s/g, '\u00A0'),
+        )
+        .replace(/([0-9])\s%/, '$1\u00A0%')
+        .replace(/(\s|^)([_A-Za-z0-9\u0100-\u017F])(\s)([_A-Za-z0-9\u0100-\u017F])/g, '$1$2\xA0$4');
 }
