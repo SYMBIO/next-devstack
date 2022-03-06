eff8f4fb003690fbb09acf3efd2809cd4acd28f5
Martin Cajthaml <martin.cajthaml@symbio.agency>
2022-03-07 00:59:25 +0100

    Update index.ts

--- packages/headless/lib/routing/index.ts
+++ packages/headless/lib/routing/index.ts
@@ -3,5 +3,6 @@ export * from './getLinkParamsFromPage';
 export * from './getLinkParamsFromUrl';
 export * from './getPagePattern';
 export * from './getRouteParamsFromUrl';
+export * from './getUrlFromPage';
 export * from './isInternalLink';
 export * from './LevelUpRedirect';
