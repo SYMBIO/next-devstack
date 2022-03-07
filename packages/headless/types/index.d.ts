5e0edf14009afae8586bd72c118a20b0253bebaa
Martin Cajthaml <martin.cajthaml@symbio.agency>
2022-03-07 01:09:22 +0100

    Fixed types

--- packages/headless/types/index.d.ts
+++
@@ -1,8 +0,0 @@
-export * from './image';
-export * from '../lib/constants';
-export * from '../lib/blocks';
-export * from '../lib/logger';
-export { AppStore } from '../lib/store/AppStore';
-
-import './scss';
-import './svg';
